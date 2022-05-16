6368b4e078e6d33ca3c12d1643409436c2f7a093
WIN-N989F684TMS\student <2022java42-student009@la-study.com>
2022-05-16 14:44:44 +0900

    laフォルダ　追加　testファイル

---
+++ src/main/java/la/bean/Testbean.java
@@ -0,0 +1,13 @@
+package la.bean;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Testbean
+ */
+@WebServlet("/Testbean")
+public class Testbean extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/la/dao/Testdao.java
@@ -0,0 +1,13 @@
+package la.dao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class Testdao
+ */
+@WebServlet("/Testdao")
+public class Testdao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
