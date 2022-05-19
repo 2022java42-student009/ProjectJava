b5d3fa76e8007d2bb59dd3626cc2fc738660836a
WIN-N989F684TMS\student <2022java42-student009@la-study.com>
2022-05-19 14:15:44 +0900

    DAO　サーブレット　一時的更新　金澤

---
+++ src/main/java/la/servlet/AdminloginServlet.java
@@ -0,0 +1,29 @@
+package la.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AdminloginServlet")
+public class AdminloginServlet extends HttpServlet {
+       
+    public AdminloginServlet() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
