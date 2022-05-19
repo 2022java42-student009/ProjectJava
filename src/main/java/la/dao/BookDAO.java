c441b7bac7a6aceabf928472dcf4e1791db85e39
WIN-L12S4MNAE5H\student <2022java42-student003@la-study.com>
2022-05-19 14:16:37 +0900

    BookDAO 全ての本を取得するメソッド追加　新井

--- src/main/java/la/dao/BookDAO.java
+++ src/main/java/la/dao/BookDAO.java
@@ -15,7 +15,7 @@ public class BookDAO {
 	private String url = "jdbc:postgresql:projectjava";
 	private String user = "postgres";
 	private String pass = "himitu";
-	
+
 	public BookDAO() throws DAOException {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -24,12 +24,39 @@ public class BookDAO {
 			e.printStackTrace();
 			throw new DAOException("JDBCドライバの登録に失敗しました。");
 		}
-		
+
 	}
-	
-	public List<CategoryBean> findAllCategory(int categoryId) throws DAOException{
+
+//	全ての本を取得
+	public List<BookBean> findAllBook() throws DAOException {
+		String sql = "SELECT * FROM book";
+
+		try (Connection con = DriverManager.getConnection(url, user, pass);
+				PreparedStatement st = con.prepareStatement(sql);
+				ResultSet rs = st.executeQuery();) {
+			List<BookBean> list = new ArrayList<BookBean>();
+			while (rs.next()) {
+				int isbm = rs.getInt("book_number");
+				String title = rs.getString("book_title");
+				int category = rs.getInt("category_id");
+				String author = rs.getString("author");
+
+				BookBean bean = new BookBean(isbm, title, category, author);
+				list.add(bean);
+			}
+			return list;
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		}
+
+	}
+
+	// カテゴリーの取得
+	public List<CategoryBean> findAllCategory() throws DAOException {
 		String sql = "SELECT * FROM bookcategory ORDER BY category_id";
-		
+
 		try (Connection con = DriverManager.getConnection(url, user, pass);
 				PreparedStatement st = con.prepareStatement(sql);
 				ResultSet rs = st.executeQuery();) {
@@ -46,37 +73,38 @@ public class BookDAO {
 			e.printStackTrace();
 			throw new DAOException("レコードの取得に失敗しました。");
 		}
-		
+
 	}
-	
-	public List<BookBean> findByCategory(int categoryId) throws DAOException{
-		String sql = "SELECT * FROM book WHERE category_code = ? ORDER BY category_id";
-		
+
+	// カテゴリーから絞り込み
+	public List<BookBean> findByCategory(int categoryId) throws DAOException {
+		String sql = "SELECT * FROM book WHERE category_id = ? ORDER BY category_id";
+
 		try (Connection con = DriverManager.getConnection(url, user, pass);
-				PreparedStatement st = con.prepareStatement(sql);){
-				st.setInt(1, categoryId);
-				try (ResultSet rs = st.executeQuery();) {
-					List<BookBean> list = new ArrayList<BookBean>();
-					while (rs.next()) {
-						int isbm = rs.getInt("book_number");
-						String title = rs.getString("book_title");
-						int category = rs.getInt("category_id");
-						String author = rs.getString("author");
-						
-						BookBean bean = new BookBean(isbm, title, category, author);
-						list.add(bean);
-					}
-					return list;
-				} catch (SQLException e) {
-					// TODO: handle exception
-					e.printStackTrace();
-					throw new DAOException("レコードの取得に失敗しました。");
+				PreparedStatement st = con.prepareStatement(sql);) {
+			st.setInt(1, categoryId);
+			try (ResultSet rs = st.executeQuery();) {
+				List<BookBean> list = new ArrayList<BookBean>();
+				while (rs.next()) {
+					int isbm = rs.getInt("book_number");
+					String title = rs.getString("book_title");
+					int category = rs.getInt("category_id");
+					String author = rs.getString("author");
+
+					BookBean bean = new BookBean(isbm, title, category, author);
+					list.add(bean);
 				}
+				return list;
+			} catch (SQLException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				throw new DAOException("レコードの取得に失敗しました。");
+			}
 		} catch (SQLException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 			throw new DAOException("レコードの取得に失敗しました。");
 		}
-		
+
 	}
 }
